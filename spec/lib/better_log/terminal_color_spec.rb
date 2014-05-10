8e955197884033a2767614062acfeb7205eb9e3a
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2014-05-10 17:01:54 -0300

    add the black text on white background sample in the terminal color spec

--- spec/lib/better_log/terminal_color_spec.rb
+++ spec/lib/better_log/terminal_color_spec.rb
@@ -15,14 +15,17 @@ describe BetterLog::TerminalColor do
     it 'explicit white text on black background' do
       expect( subject.colorize('pretty text', color: :white, bg_color: :black) ).to eq("\e[37;40mpretty text\e[0m")
     end
+    it 'black text on white background' do
+      expect( subject.colorize('pretty text', color: :black, bg_color: :white) ).to eq("\e[30;47mpretty text\e[0m")
+    end
     it 'red text on green background' do
       expect( subject.colorize('pretty text', color: :red, bg_color: :green) ).to eq("\e[31;42mpretty text\e[0m")
     end
-    it 'cyan text on magenta background' do
-      expect( subject.colorize('pretty text', color: :cyan, bg_color: :magenta) ).to eq("\e[36;45mpretty text\e[0m")
-    end
     it 'yellow text on blue background' do
       expect( subject.colorize('pretty text', color: :yellow, bg_color: :blue) ).to eq("\e[33;44mpretty text\e[0m")
+    end
+    it 'cyan text on magenta background' do
+      expect( subject.colorize('pretty text', color: :cyan, bg_color: :magenta) ).to eq("\e[36;45mpretty text\e[0m")
     end
   end
 end
